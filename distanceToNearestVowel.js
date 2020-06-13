119e528e69a141b8cde1b5fa61ba2c004073e590
Ashutosh Biswas <ashutoshbw314@gmail.com>
2020-06-13 12:15:59 +0600

    Solved an expert problem on edabit.

---
+++ distanceToNearestVowel.js
@@ -0,0 +1,6 @@
+function distanceToNearestVowel(str) {
+  const vowelsRegex = /[aeiou]/;
+  const searchForward = i => str.slice(i).search(vowelsRegex);
+  const searchBackward = i => [...str.slice(0, i + 1)].reverse().join("").search(vowelsRegex);
+  return [...str].map((_, i) => Math.min(...[searchBackward(i), searchForward(i)].filter(v => v != -1)));  
+}

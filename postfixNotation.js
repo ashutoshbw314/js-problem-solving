0df7ff5bea7191f17e75f114117d853a94251b9d
Ashutosh Biswas <ashutoshbw314@gmail.com>
2020-06-20 16:27:18 +0600

    Solved the postfix notation challenge from edabit

---
+++ postfixNotation.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/JxM8RyFw42TraEyfN
+
+function postfix(expr) {
+  let operations = { 
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b
+  }
+  return (!/ /.test(expr)) ? +expr : postfix(expr.replace(/(\d+) (\d+) ([+\-*/])/, (_, a, b, s) => operations[s](+a, +b)));
+}
